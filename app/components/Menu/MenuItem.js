c07df787a99de8091d439ddee80fb8b330cf03d7
Bram <brampijper@gmail.com>
2023-08-20 17:12:39 +0200

    deleted unneeded folders

--- app/components/Menu/MenuItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function MenuItem({ children, styles }) {
-    return (
-        <div className={`text-gray-900 p-2 min-w-[100px] text-sm flex select-none ${styles}`}>
-            {children}
-        </div>
-    )
-}
